f887d170da48f2baf37267854ecaf52b17102c2d
tincann <saabnl@gmail.com>
2014-11-12 17:55:00 +0100

    opzetje CommonApi service

--- services/commonapi.js
+++ services/commonapi.js
@@ -1,1 +1,19 @@
 var http = require('http');
+
+function CommonApi(){
+
+}
+
+
+CommonApi.prototype.getClients = function() {
+	var options = {
+		method: 'POST',
+		
+	};
+
+	http.request(options, function(response){
+
+	});
+};
+
+module.exports = new CommonApi();
