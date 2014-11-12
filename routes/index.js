e11bba34fdf8c08c420c0e0ce4ff753815ad558c
jona <jonaderksen@gmail.com>
2014-11-12 19:50:49 +0100

    probeersel
    probeersel jona

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'about' });
+});
+
+/* GET contact page */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'contact' });
+});
+
+/* GET user page */
+router.get('/user', function(req, res) {
+  res.render('user', { title: 'user' });
+});
+
+/* GET login page */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login' });
+});
+
 module.exports = router;
