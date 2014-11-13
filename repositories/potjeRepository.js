e78e4ed394bed2543edd486a3758fd9941974cd0
Erik van der Kramer <erikvanderkramer@gmail.com>
2014-11-13 02:32:31 +0100

    Lekker potjes maken met members want zo rollen wij

--- repositories/potjeRepository.js
+++ repositories/potjeRepository.js
@@ -28,31 +28,32 @@ PotjeRepository.prototype.createPotje = function(potje_data, members, callback) 
     pot_id = r1.insertId;
 
     console.log(pot_id);
+    console.log(members);
 
-    for (var i = 0; i > members.length; i++)
+    var inserts = [];
+
+    console.log(members.length);
+
+    for (var i = 0; i < members.length; i++)
     {
-      var x = {
-        potje_id : pot_id,
-        account_id : members[i]
-      }
-
-      console.log(x);
-      connection.query('INSERT INTO potje_has_accounts SET ?', x, function(errors,r2,nogiets){
-        console.log(r2);
-        callback(r2);
-      }) 
+      var x = [pot_id, members[i].account_id];
+      inserts.push(x);
     }
+
+    connection.query('INSERT INTO `potje_has_accounts` (potje_id, account_id) VALUES ?', [inserts], function(errors,r2,nogiets){
+      callback(r);
+      connection.end();
+    });
   })
-
-  connection.end();
 };
-
 
 module.exports = new PotjeRepository();
 
-
+/*
 var c = new PotjeRepository();
 
 c.createPotje({name:"lekkerpotjehoor"},[{account_id:2},{account_id:3},{account_id:4}],function(data){
   console.log(data);
 });
+
+*/
