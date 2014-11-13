7ab15ffdaac659fd762b376eded270be7b8fd2b2
daanrijpkema <info@daanrijpkema.com>
2014-11-13 04:19:18 +0100

    updates aan create form en autcomplete library (form werkt nog niet helemaal, autocomplete als het goed is wel. TODO: autocomplete moet dictionary lezen via ajax call

--- public/scripts/potje-angular.js
+++ public/scripts/potje-angular.js
@@ -3,35 +3,38 @@ angular.module('potjeApp', [])
   .controller('PotjeController', ['$scope', function($scope) {
     $scope.People = [
       {id:"0",text:'Henk de Vries', status:false},
-      {id:"1",text:'Dirk Thierens', status:false}
+      // {id:"1",text:'Dirk Thierens', status:false}
   	];
  	
  	$scope.nameText ='';
 
-    $scope.addPerson = function() {
-      if($scope.nameText!='') {
-      	$scope.People.push({id:$scope.People.length+1,text:$scope.nameText, accountNumber:false});
-      	$scope.nameText = '';
-      	$('#addPersonButton').removeClass('btn-primary');
-      } 
-      // else {
-      	// toastr.error("Je moet een naam invullen voordat je een persoon kan toevoegen");
-      // }
-    };
+  $scope.addPerson = function() {
+    $scope.nameText = $('#addPersonField').val();
+    if($scope.nameText!='') {
+    	$scope.People.push({id:$scope.People.length+1,text:$scope.nameText, accountNumber:false});
+    	$scope.nameText = '';
+    	$('#addPersonButton').removeClass('btn-primary');
+    } 
+    // else {
+    	// toastr.error("Je moet een naam invullen voordat je een persoon kan toevoegen");
+    // }
+  };
 
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.People, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
- 
-    $scope.archive = function() {
-      var oldPeople = $scope.People;
-      $scope.People = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
-    };
-  }]);
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.People, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.archive = function() {
+    var oldPeople = $scope.People;
+    $scope.People = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+
+
+}]);
--- public/scripts/potje-jquery.js
+++ public/scripts/potje-jquery.js
@@ -9,4 +9,41 @@ $(document).ready(function() {
 			$('#addPersonButton').addClass('btn-primary');
 		}
 	});
+
+	// $('.autocomplete').autocomplete('disable');
+	var adresboek = [
+	   { value: 'Henk', data: '0' },
+	   { value: 'Bob', data: '1' }
+	];
+
+
+	$('.autocomplete').autocomplete({
+	    lookup: adresboek
+	});
+	    // onSelect: function (suggestion) {
+	    //     alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+	    // }
+
+	$("#createNewPotjeButton").click(function(){
+	    console.log("nieuw potje aanmaken");
+
+	    var potje = {};
+	    //inlezen form data
+	    potje.naam = $('input[name="accountName"]').val();
+
+
+
+
+	    potje.inlegBedrag = $('input[name="inlegBedrag"]').val();
+
+
+	    //optioneel: valideren
+
+
+	    //posten!	    
+		$.post("create/potje", potje);
+
+		
+
+  	});
 });
